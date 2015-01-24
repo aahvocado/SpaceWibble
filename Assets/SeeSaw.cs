1daedab92da9166698cf43a36fe3f39177c69875
neonwednesdays <nxiao@macrun.local>
2015-01-23 23:46:11 -0500

    Welcome to SpaceWibble!
    48hr Game Jam for Global Game Jam Spring 2015 where the theme is “What
    do we do now?”

---
+++ Assets/SeeSaw.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeeSaw : MonoBehaviour {
+	public GameObject seesaw;
+
+	private Vector3 centerPoint;
+	public float rotationPower;
+	// Use this for initialization
+	void Start () {
+		centerPoint = seesaw.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider col){
+		if (col.gameObject.tag == "AffectSeesaw") {
+			Vector3 objPos = col.gameObject.transform.position;
+			float dist = centerPoint.z - objPos.z;
+			float dir = dist > 0 ? -1:1;
+
+//			seesaw.transform.Rotate(new Vector3(0,0,zDiff * rotationPower) * Time.deltaTime );
+			Vector3 axis = new Vector3(20*dir,0,0);
+			seesaw.transform.RotateAround(centerPoint,axis, 10*Time.deltaTime);
+			Debug.Log ("ouch " + dist);
+
+		}
+	}
+
+}
+
+
