45f0f8998ae0a6a6d3d15ef7b716647ec1076455
neonwednesdays <nxiao@lawn-128-61-123-74.lawn.gatech.edu>
2015-01-24 18:14:25 -0500

    asteroid and particle effects, wall detection
    Asteroid Test scene

---
+++ SeeSawTest/Assets/Scripts/flyingAsteroidClass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyingAsteroidClass : MonoBehaviour {
+	public Vector3 initialVelocity;
+	public GameObject explodeParticle;
+	// Use this for initialization
+	void Start () {
+//		initialVelocity = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(initialVelocity * Time.deltaTime, Space.World);
+		transform.Rotate (new Vector3 (6, 6, 6) * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Wall") {
+			Destroy(this.gameObject);
+
+			Instantiate(explodeParticle, this.transform.position, Quaternion.identity);
+		}
+	}
+}
